29c4bf9888c2fd1a2b2432138fde6490a9437a4b
Gilberto-Felipe <gramirez25@ucol.mx>
2019-04-30 09:56:51 -0500

    arreglo a funcionalidad en jugador.mover
    arreglé problema para que funcionen casillas serpientes y escaleras

--- jugador.js
+++ jugador.js
@@ -16,10 +16,12 @@ class Jugador {
         return this._posicion;
     }
 
-    mover = (posicion) => {
+    mover = (posicion, casillas) => {
         this._posicion += posicion;
+        if (casillas[this._posicion] !== casillas + 1)
+            this._posicion = casillas[this._posicion] - 1;
         console.log(this._nombre, 'avanza a la casilla');
-        console.log(this._posicion);
+        console.log(casillas[this._posicion]);
                 
     }
 }
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // creo los objetos
 let tablero = new Tablero();
-console.log(tablero.Casillas);
+//console.log(tablero.Casillas);
 
 let dado = new Dado();
 let jugadorA = new Jugador('Pedro');
 let jugadorB = new Jugador('Masha');
 
 // el jugador que llegue primero a la casilla 100 gana
-while (jugadorA.Posicion < 100 && jugadorB.Posicion < 100) {
+while (jugadorA.Posicion < 99 && jugadorB.Posicion < 99) {
     // el juagdorA ubica su posición, lanza el dado y avanza
     jugadorA.Posicion;
-    jugadorA.mover(dado.lanzar());
+    jugadorA.mover(dado.lanzar(), tablero.Casillas);
     //jugadorA.Posicion = tablero.Casillas[contador];
 
     // el jugadorB lanza el dado y avanza
     jugadorB.Posicion;
-    jugadorB.mover(dado.lanzar());
+    jugadorB.mover(dado.lanzar(), tablero.Casillas);
 }
 
